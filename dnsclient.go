7775db944b4bdef0efdd240b5fb1b0a5911029ef
Ali Mosajjal <hi@n0p.me>
2022-10-19 23:37:42 +1300

    better readme and docs

--- dnsclient.go
+++ dnsclient.go
@@ -16,19 +16,42 @@ import (
 	"github.com/miekg/dns"
 )
 
-// Client Provides a unified interface for DNS queries
+/*
+Client Provides a unified interface for DNS queries
+
+	func main() {
+		msg := dns.Msg{}
+		msg.RecursionDesired = true
+		msg.Question = []dns.Question{{
+			Name:   "example.com.",
+			Qtype:  dns.StringToType["A"],
+			Qclass: dns.ClassINET,
+		}}
+
+		addr := &net.UDPAddr{
+			IP:   net.IPv4(1, 1, 1, 1),
+			Port: 53,
+		}
+		c, _ := dnsclient.NewClassicDNS(addr, false, false, false)
+		response, ttr, err := c.Query(context.Background(), &msg)
+		fmt.Printf("Query: %v, Response: %v, Time to Respond: %s, Error: %v", msg, response, ttr, err)
+	}
+*/
 type Client interface {
 	Query(context.Context, *dns.Msg) ([]dns.RR, time.Duration, error)
 }
 
-// New creates a DNS Client by parsing a URI and returning the appropriate client for it
-// URI string could look like below:
-// udp://1.1.1.1:53
-// udp6://[2606:4700:4700::1111]:53
-// tcp://9.9.9.9:5353
-// https://dns.adguard.com
-// quic://dns.adguard.com:8853
-// tls://dns.adguard.com:853
+/*
+New creates a DNS Client by parsing a URI and returning the appropriate client for it
+URI string could look like below:
+
+  - udp://1.1.1.1:53
+  - udp6://[2606:4700:4700::1111]:53
+  - tcp://9.9.9.9:5353
+  - https://dns.adguard.com
+  - quic://dns.adguard.com:8853
+  - tls://dns.adguard.com:853
+*/
 func New(uri string, skipVerify bool) (Client, error) {
 	parsedURL, err := url.Parse(uri)
 	if err != nil {
